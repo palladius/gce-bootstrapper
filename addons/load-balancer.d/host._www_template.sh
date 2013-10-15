1de8e0e1538c48726ddcf9ea71e38e5a29d61ecf
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-10-15 23:49:16 +0200

    bgcolor on all4  pages, for session affinity

--- addons/load-balancer.d/host._www_template.sh
+++ addons/load-balancer.d/host._www_template.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-VER=1.1a
+VER=1.2
 
 apt-get install -y apache2
 
 COLOR=$(getmetadata color)
 BGCOLOR=$(curl http://metadata/0.1/meta-data/attributes/bgcolor)
 
-echo "<h1>Welcome to $HOSTNAME</h1> Try also <a href='index2'.html >index2</a> or  <a href='index3'.html >index3</a> to test Session Affinity." > /var/www/index4.html
-echo "<h1>Index2 for $HOSTNAME</h1> Go back to <a href='/index'.html >index</a>." > /var/www/index2.html
-echo '<!doctype html><html><body><h1>Hello World 3!</h1></body></html>' | sudo tee /var/www/index3.html
+echo "<html><body bgcolor='$BGCOLOR' ><h1>Welcome to $HOSTNAME</h1> Try also <a href='index2'.html >index2</a> or  <a href='index3'.html >index3</a> to test Session Affinity." > /var/www/index4.html
+echo "<html><h1>Index2 for $HOSTNAME</h1><body bgcolor='$BGCOLOR'> Go back to <a href='/index'.html >index</a>." > /var/www/index2.html
+echo "<!doctype html><html><body bgcolor=\"$BGCOLOR\" ><h1>Hello World 3!</h1></body></html>" | sudo tee /var/www/index3.html
 
 cat > /var/www/index.html <<EOF
 <!doctype html>
