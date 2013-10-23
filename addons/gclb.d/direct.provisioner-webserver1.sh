39153d30c78f9cb100279895f5fd1eab2c85f3f7
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-10-23 18:33:51 +0200

    it WORKS!!!

--- addons/gclb.d/direct.provisioner-webserver1.sh
+++ addons/gclb.d/direct.provisioner-webserver1.sh
@@ -1,27 +1,32 @@
 # host : webserver1/2/3.sh
 
 BGCOLOR=$(curl http://metadata/0.1/meta-data/attributes/bgcolor)
-VER='1.0'
+VER='1.1'
 
 apt-get install -y apache2
 
 cat > /var/www/index.html <<EOF
 <html>
  <body bgcolor='$BGCOLOR' >
-  <h1>Welcome to $HOSTNAME</h1> 
-  <p>Testing Load Balancer with BGCOLOR=$BGCOLOR 'alla vecchia'.</p>
+  <h1>First: $HOSTNAME</h1> 
+  <p>Testing Load Balancer simply with BGCOLOR=$BGCOLOR.</p>
+
+  <p>Click on <a href='/index.html'  >index.html</a>.  </p>
+  <p>Click on <a href='/index2.html' >index2.html</a>. </p>
  </body>
 </html>
 EOF
+
+cat /var/www/index.html | sed -e s/First/Second/g > /var/www/index2.html
+
 hostname > /var/www/hostname
 
-chmod 755 /var/www/index.html
+chmod 755 /var/www/index.html /var/www/index2.html /var/www/hostname
 
 # stops apache on www5 to test Load Balancer
-if /bin/hostname | grep -q www5 ; then
+if /bin/hostname | egrep -q "www5|www1" ; then
     touch /root/apache-stopping.touch
-    service apache2 stop &&
-        touch /root/apache-stopped-ok.touch
+    service apache2 stop && touch /root/apache-stopped-ok.touch
 fi
 
 touch /root/installed-gclb-addon-v$VER.touch
--- addons/gclb.sh
+++ addons/gclb.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
-VER=1.0
+VER=1.2
 
 PROJ="google.com:biglamp"
 REGION='europe-west1'
 ZONE0='europe-west1-a'
 ZONE1='europe-west1-b'
-PREFIX='test-gclb2-'
+PREFIX=${1:-just-test-}
 
+
+PREFIX="${PREFIX}-"       # Adding a dash always :)
 GCUTIL="gcutil --project=$PROJ"
 # How to configure GCLB:
 # https://cloud.google.com/resources/articles/google-compute-engine-load-balancing-in-action
 set -e
 set -x
 
-gcutil_add() {
-    vm="$1"
-    zone="$2"
-    $GCUTIL addinstance ${PREFIX}$vm --description "Test WWW machine to demonstrate GCLB, created by $0 v$VER" \
-      --zone $zone --tags gclb,test,deleteme \
+function _gcutil_add() {
+    VM="$1"
+    ZONE="$2"
+    COLOR="$3"
+
+    $GCUTIL addinstance "${PREFIX}$VM" \
+      --description "Test WWW machine to demonstrate GCLB, created by $0 v$VER" \
+      --zone $ZONE \
+      --tags gclb,test,deleteme,webbable \
       --machine_type n1-standard-1 \
+      --metadata bgcolor="$COLOR" \
+      --metadata provisioning_script="$0 v$VER" \
       --nopersistent_boot_disk \
       --image debian-7 \
-      --metadata_from_file=startup-script:gclb.d/direct.provisioner-webserver1.sh
+      --metadata_from_file=startup-script:gclb.d/direct.provisioner-webserver1.sh &
 
 }
 
-gcutil_add www0 $ZONE0
-gcutil_add www1 $ZONE0
-gcutil_add www2 $ZONE1
-gcutil_add www3 $ZONE1
+# Configuring Firewall:
+$GCUTIL addfirewall ${PREFIX}open-80-and-icmp-to-all --allowed 'tcp:22,tcp:80,icmp' &
 
-sleep 2
+_gcutil_add www0 $ZONE0 yellow
+_gcutil_add www1 $ZONE0 green
+_gcutil_add www2 $ZONE1 red
+_gcutil_add www3 $ZONE1 blue
 
-$GCUTIL ssh ${PREFIX}www0 "sudo service apache2 stop" &
+# sleep 2
 
-$GCUTIL addhttphealthcheck basic-check   #checks on port 80 trivially, no parametric name
-$GCUTIL addtargetpool      ${PREFIX}www-tp --region=$REGION --instances=$ZONE0/${PREFIX}www0,$ZONE0/${PREFIX}www1,$ZONE1/${PREFIX}www2,$ZONE1/${PREFIX}www3 --health_checks="basic-check"
-$GCUTIL addforwardingrule  ${PREFIX}www-fwdrule --region=$REGION --port_range=80 --target=${PREFIX}www-tp
+# Stopping apache on first instance, to check TP Health
+$GCUTIL ssh "${PREFIX}www0" "sudo service apache2 stop" &
 
-yellow "you might weant to try: $GCUTIL gettargetpoolhealth www-tp"
+# Configuring GCLB
+$GCUTIL addhttphealthcheck "${PREFIX}basic-check"   # checks on port 80 trivially, no parametric name
+$GCUTIL addtargetpool      "${PREFIX}www-tp" --region=$REGION --instances "$ZONE0/${PREFIX}www0,$ZONE0/${PREFIX}www1,$ZONE1/${PREFIX}www2,$ZONE1/${PREFIX}www3" --health_checks="${PREFIX}basic-check"
+$GCUTIL addforwardingrule  "${PREFIX}www-fwdrule" --region=$REGION --port_range=80 --target=${PREFIX}www-tp
+
+echo Health:
+$GCUTIL gettargetpoolhealth "${PREFIX}www-tp" # | tee .targetpoolhealth.${PREFIX}www-tp.tmp
+
+echo All good.
